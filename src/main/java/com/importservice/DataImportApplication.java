aaab1c12f49cbc60822c426e6acf2a5489531ed1
M-Fararjeh <moath.fararjeh@gmail.com>
2025-08-30 14:44:01 +0300

    Fix Spring Boot application configuration

--- src/main/java/com/importservice/DataImportApplication.java
+++ src/main/java/com/importservice/DataImportApplication.java
@@ -2,10 +2,8 @@ package com.importservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.importservice")
 public class DataImportApplication {
 
     public static void main(String[] args) {
