3fbd24290121421247120c5b7ccdd5ea4a1a84c9
M-Fararjeh <moath.fararjeh@gmail.com>
2025-08-30 14:53:52 +0300

    Fix missing @Value annotation import

--- src/main/java/com/importservice/controller/ImportController.java
+++ src/main/java/com/importservice/controller/ImportController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
