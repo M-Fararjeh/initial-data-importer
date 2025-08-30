8b0fe82f01df6f8c17641ac267524288da7620bb
M-Fararjeh <moath.fararjeh@gmail.com>
2025-08-30 14:42:58 +0300

    Fix Spring Boot component scanning

--- src/main/java/com/importservice/DataImportApplication.java
+++ src/main/java/com/importservice/DataImportApplication.java
@@ -2,8 +2,10 @@ package com.importservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.importservice")
 public class DataImportApplication {
 
     public static void main(String[] args) {
