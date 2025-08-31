73f61223349e69485e21ec7619f079a981ce43c7
M-Fararjeh <moath.fararjeh@gmail.com>
2025-09-01 02:45:46 +0300

    Fix Zone.js requirement for Angular

--- angular-migration-ui/src/app/app.module.ts
+++ angular-migration-ui/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { MigrationDashboardComponent } from './components/migration-dashboard/migration-dashboard.component';
