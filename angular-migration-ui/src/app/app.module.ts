67160c142949d7ac55c4656ba93073b335fe6edb
M-Fararjeh <moath.fararjeh@gmail.com>
2025-09-01 15:45:42 +0300

    Fix missing comma in app.module.ts

--- angular-migration-ui/src/app/app.module.ts
+++ angular-migration-ui/src/app/app.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
     MigrationDashboardComponent,
     PhaseCardComponent,
     StatisticsCardComponent,
-    CreationDetailsComponent
+    CreationDetailsComponent,
     AssignmentDetailsComponent
   ],
   imports: [
