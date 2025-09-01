2824c8b31b7e877c44b0cbeb2eb597a40c569647
M-Fararjeh <moath.fararjeh@gmail.com>
2025-09-01 15:44:45 +0300

    Fix missing Arrays import in DestinationSystemService

--- src/main/java/com/importservice/service/DestinationSystemService.java
+++ src/main/java/com/importservice/service/DestinationSystemService.java
@@ -28,6 +28,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
