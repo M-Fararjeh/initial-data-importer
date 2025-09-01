18c39095cb9305375b1a7df1b2fa63a4fd3ddc72
M-Fararjeh <moath.fararjeh@gmail.com>
2025-09-01 03:07:45 +0300

    Fix duplicate CommonModule import

--- angular-migration-ui/src/app/app.module.ts
+++ angular-migration-ui/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
