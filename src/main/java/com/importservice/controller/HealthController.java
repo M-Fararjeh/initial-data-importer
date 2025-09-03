a7be7e891cc19a85b5b77ef16e37118fca46f461
M-Fararjeh <moath.fararjeh@gmail.com>
2025-09-03 14:42:56 +0300

    test

--- src/main/java/com/importservice/controller/HealthController.java
+++ src/main/java/com/importservice/controller/HealthController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.importservice.util.CorrespondenceSubjectGenerator;
 import com.importservice.service.KeycloakTokenService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
