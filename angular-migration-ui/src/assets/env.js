3de41489105bfb123bf19168366768548b7492d9
M-Fararjeh <moath.fararjeh@gmail.com>
2025-09-06 19:02:57 +0300

    Enhancements

--- angular-migration-ui/src/assets/env.js
+++
@@ -1,7 +0,0 @@
-// Environment configuration for runtime
-(function(window) {
-  window["env"] = window["env"] || {};
-
-  // Environment variables - these will be replaced by Docker at runtime
-  window["env"]["API_BASE_URL"] = "http://localhost:8080";
-})(this);
